8f9a7f3bfcd431e3ae1b8f73fc3d1fd7d511d3c6
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 16:16:41 +0700

    menambahkan class KodeProduk

---
+++ jurnal4/KodeProduk.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnal4
+{
+    class KodeProduk
+    {
+
+
+        private Dictionary<string, string> kodeProduk = new Dictionary<string, string> {
+        {"Laptop", "E100"},
+        {"Smartphone", "E101"},
+        {"Tablet", "E102"},
+        {"Headset", "E103"},
+        {"Keyboard", "E104"},
+        {"Mouse", "E105"},
+        {"Printer", "E106"},
+        {"Monitor", "E1007"},
+        {"Smartwatch", "E108"},
+        {"Kamera", "E109"}
+        
+    };
+
+        public string getKodeProduk(string namaProduk)
+        {
+            return kodeProduk.ContainsKey(namaProduk) ? kodeProduk[namaProduk] : "Kode Produk Tidak Ditemukan";
+        }
+    }
+}
--- jurnal4/Program.cs
+++ jurnal4/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using jurnal4;
+
+class Program
+{
+    static void Main()
+    {
+        KodeProduk kodeProduk = new KodeProduk();
+
+        Console.Write("Masukkan nama Barang: ");
+        string namaBarang = Console.ReadLine();
+
+        Console.WriteLine("Kode Produk: " + kodeProduk.getKodeProduk(namaBarang));
+       
+
+        
+    }
+}
